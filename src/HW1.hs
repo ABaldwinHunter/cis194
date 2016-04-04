a264146c291c024889caf3a7bb30b740eb7c81ef
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2016-04-03 23:35:54 -0400

    move code to source directory and create empty test directory

--- src/HW1.hs
+++ src/HW1.hs
@@ -1,9 +1,9 @@
-toDigits :: Integer -> [Integer]
+toDigits :: Int -> [Int]
 toDigits n
   | n <= 0 = []
   | otherwise = toDigits (n `div` 10) ++ [n `mod` 10]
 
-toDigitsRev :: Integer -> [Integer]
+toDigitsRev :: Int -> [Int]
 toDigitsRev n = reverse (toDigits n)
 
 mapWithIndex :: (a -> Int -> b) -> [a] -> [b]
@@ -22,14 +22,17 @@ doubleEveryOther n = reverse (mapWithIndex applyDoubling (reverse n))
       | (i `mod` 2) == 0 = n
       | otherwise = 2 * n
 
-sumDigits :: [Integer] -> Integer
+sumDigits :: [Int] -> Int
 sumDigits [] = 0
 sumDigits n = sumValues (allTheSingleLadies n)
   where
-    allTheSingleLadies :: [Integer] -> [Integer]
+    allTheSingleLadies :: [Int] -> [Int]
     allTheSingleLadies [] = []
     allTheSingleLadies (x:xs) = (toDigits x) ++ (allTheSingleLadies xs)
 
-    sumValues :: [Integer] -> Integer
+    sumValues :: [Int] -> Int
     sumValues [] = 0
     sumValues (x:xs) = x + sumValues xs
+
+validate :: Int -> Bool
+validate y = sumDigits (doubleEveryOther (toDigits y)) `mod` 10 == 0
